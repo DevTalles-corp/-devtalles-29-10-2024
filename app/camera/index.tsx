d46cca265c14054dfaf4a74e040838904937f463
Fernando Herrera <fernando.herrera85@gmail.com>
2024-10-28 10:45:59 -0400

    Refactor app components and add camera icon to header

--- app/camera/index.tsx
+++ app/camera/index.tsx
@@ -1,5 +1,21 @@
+import MenuIconButton from '@/components/MenuIconButton';
+import { router, useNavigation } from 'expo-router';
+import { useEffect } from 'react';
 import { View, Text } from 'react-native';
 const CameraScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <MenuIconButton
+          onPress={() => router.push('/camera')}
+          icon="camera-outline"
+        />
+      ),
+    });
+  }, []);
+
   return (
     <View>
       <Text>CameraScreen</Text>
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,7 +3,7 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#d00000';
+const tintColorLight = '#4d1398';
 const tintColorDark = '#fff';
 
 export const Colors = {
