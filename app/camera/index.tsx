d9df135b5497a9197a1400260f54996ac12afff2
Fernando Herrera <fernando.herrera85@gmail.com>
2024-10-28 11:24:11 -0400

    Refactor camera component and add image gallery functionality

--- app/camera/index.tsx
+++ app/camera/index.tsx
@@ -1,9 +1,12 @@
 import MenuIconButton from '@/components/MenuIconButton';
+import { useNativeResourcesStore } from '@/store/native-resources.store';
 import { router, useNavigation } from 'expo-router';
 import { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, FlatList } from 'react-native';
+
 const CameraScreen = () => {
   const navigation = useNavigation();
+  const { images } = useNativeResourcesStore();
 
   useEffect(() => {
     navigation.setOptions({
@@ -17,8 +20,19 @@ const CameraScreen = () => {
   }, []);
 
   return (
-    <View>
-      <Text>CameraScreen</Text>
+    <View style={{ marginHorizontal: 5 }}>
+      <FlatList
+        numColumns={2}
+        data={images}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Image
+            source={{ uri: item }}
+            style={{ width: '50%', height: 200, margin: 5 }}
+            resizeMode="cover"
+          />
+        )}
+      />
     </View>
   );
 };
