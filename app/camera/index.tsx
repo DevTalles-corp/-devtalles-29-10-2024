d2f235209235910595fcb1e27ac39edddc019f92
Fernando Herrera <fernando.herrera85@gmail.com>
2024-10-28 09:42:33 -0400

    Refactor app components and update colors

---
+++ app/camera/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const CameraScreen = () => {
+  return (
+    <View>
+      <Text>CameraScreen</Text>
+    </View>
+  );
+};
+export default CameraScreen;
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const NativeApp = () => {
+  return (
+    <View>
+      <Text>NativeApp</Text>
+    </View>
+  );
+};
+export default NativeApp;
